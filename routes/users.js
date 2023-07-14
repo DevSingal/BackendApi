b1b72d913b9d1b389fa15031a8ba4c0fffe20080
Dev Singal <devsingal2000@gmail.com>
2023-07-14 07:25:58 +0530

    updated user routes

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,7 @@ router.post("/register", registerUser).get((req, res) => { res.render("register"
 
 router.post("/login", loginUser).get((req, res) => { res.render("login") })
 
-<<<<<<< HEAD
 router.get("/logout",isAuthenticated,  logoutUser) 
-=======
-router.get("/logout", isAuthenticated, logoutUser) 
->>>>>>> 2786fd793dd3c383e951c78e41f3ea2fa8ae8a7d
 
 router.get("/profile",  isAuthenticated, getMyProfile )
 
